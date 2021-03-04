297024003955095b362b29d1d44e85f4833b8ca6
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2021-03-04 19:25:25 +0100

    Removes the .min extension

--- webpack.mix.js
+++ webpack.mix.js
@@ -32,14 +32,11 @@ if ( mix.inProduction() )
 
     // Purge our CSS
     // mix minifies CSS & JS by default
-    // I prefer to add the `.min` suffix on the output files just for convention
     mix.purgeCss({
             content: ['site/**/*.njk'],
             safelist: ['menu-visible', 'loaded', 'expanded', /^type-/, /^page-/, /[data-src]/],
             extractorPattern: [/[^<>"'`\s]*[^<>"'`\s:]/g]
         })
-        .minify('css/main.css')
-        .minify('js/main.js')
 
 }
 
